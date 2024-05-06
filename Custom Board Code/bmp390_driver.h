743a207b3e8ae809bc58635fa4dd5b6d9a81a96d
0xPedro123 <166990330+0xPedro123@users.noreply.github.com>
2024-05-06 13:26:33 +0200

    Update bmp390_driver.h
    Finalized defines (some missing? SD, API, MIN-MAX)
    Added function defines

--- Custom Board Code/bmp390_driver.h
+++ Custom Board Code/bmp390_driver.h
@@ -180,6 +180,15 @@
 #define BMP3_PAR_P10_ADR 0x44
 #define BMP3_PAR_P11_ADR 0x45
 
+/**\name BMP3 pressure settling time (micro secs)*/
+#define BMP3_SETTLE_TIME_PRESS                  UINT16_C(392)
+
+/**\name BMP3 temperature settling time (micro secs) */
+#define BMP3_SETTLE_TIME_TEMP                   UINT16_C(313)
+
+/**\name BMP3 adc conversion time (micro secs) */
+#define BMP3_ADC_CONV_TIME                      UINT16_C(2020)
+
 typedef struct
 {
 	I2C_HandleTypeDef *i2cHandle;
@@ -217,5 +226,39 @@ typedef struct
     float  t_lin;
 
 }BMP3_CALIB_DATA;
+
+void BMP3_initialisation(BMP3 *barometer, EEPROM *eeprom, uint8_t i2c_address,
+						 I2C_HandleTypeDef *i2cHandle,
+						 BMP3_CALIB_DATA *calib_baro1);
+
+HAL_StatusTypeDef BMP3_write_register(BMP3 *barometer, uint16_t reg,
+									  uint8_t *data);
+
+HAL_StatusTypeDef BMP3_read_register(BMP3 *barometer, uint16_t reg,
+									 uint8_t *data);
+
+HAL_StatusTypeDef BMP3_read_registers(BMP3 *barometer, uint16_t reg,
+									  uint8_t *data, uint8_t length);
+
+void BMP3_get_temperature(BMP3 *barometer,BMP3_CALIB_DATA *calib_baro);
+
+void BMP3_get_pressure(BMP3 *barometer,BMP3_CALIB_DATA *calib_baro);
+
+void BMP3_compensate_temperature(BMP3 *barometer ,BMP3_CALIB_DATA *calib_baro1,
+								 int32_t raw_temperature);
+
+void BMP3_compensate_pressure(BMP3 *barometer ,BMP3_CALIB_DATA *calib_baro,
+							  uint32_t raw_pressure);
+
+void BMP3_datalog(BMP3 *barometer, FIL *fil, FRESULT fres, char *sd_buffer,
+				  uint8_t* buffer_index, char file_name[]);
+
+void BMP3_interrupt_port_setup(BMP3 *barometer);
+
+void BMP3_interrupt_setup(BMP3 *barometer);
+
+void BMP3_trimming_coeff(BMP3 *barometer, BMP3_CALIB_DATA *calib_baro);
+
+void BMP3_setup_normal_mode(BMP3 *barometer);
 
 #endif /* INC_BMP390_DRIVER_H_ */
