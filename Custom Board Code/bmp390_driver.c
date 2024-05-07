a5c4fafc91c879eb5d83f7c8b8103fcce3d25a21
0xPedro123 <166990330+0xPedro123@users.noreply.github.com>
2024-05-07 09:20:04 +0200

    Update bmp390_driver.c
    Read and Writes completed

--- Custom Board Code/bmp390_driver.c
+++ Custom Board Code/bmp390_driver.c
@@ -4,24 +4,41 @@
  *  Created on: May 2, 2024
  *      Author: psanc
  */
-void BMP3_initialisation(BMP3 *barometer, EEPROM *eeprom, uint8_t i2c_address,
-						 I2C_HandleTypeDef *i2cHandle,
-						 BMP3_CALIB_DATA *calib_baro1) {
+void BMP3_initialisation(BMP3 *barometer, EEPROM *eeprom, uint8_t i2c_address, I2C_HandleTypeDef *i2cHandle, BMP3_CALIB_DATA *calib_baro1) {
+	barometer -> i2cHandle = i2cHandle;
+	barometer -> i2c_address = i2c_address;
+	HAL_StatusTypeDef ret;
+	ret = HAL_I2C_IsDeviceReady(barometer -> i2cHandle, barometer -> i2c_address, 1, HAL_MAX_DELAY);
+	if(ret != HAL_OK){
+		printf("FAILURE TO CONNECT TO BMP388\n");
+	}
+	BMP3_trimming_coeff(barometer, calib_baro);
+	BMP3_interrupt_setup(barometer);
+	BMP3_setup_normal_mode(barometer);
+	BMP3_interrupt_port_setup(barometer);
+	get_baro_data_eeprom(barometer, eeprom);
+	printf("BMP388 initialisation over\n");
 }
 
-HAL_StatusTypeDef BMP3_write_register(BMP3 *barometer, uint16_t reg,
-									  uint8_t *data){
+HAL_StatusTypeDef BMP3_write_register(BMP3 *barometer, uint16_t reg, uint8_t *data)
+{
+	return HAL_I2C_Mem_Write(barometer -> i2cHandle, barometer -> i2c_address, reg, I2C_MEMADD_SIZE_8BIT, data, BMP3_SIZE ,100);
 }
 
-HAL_StatusTypeDef BMP3_read_register(BMP3 *barometer, uint16_t reg,
-									 uint8_t *data){
+HAL_StatusTypeDef BMP3_read_register(BMP3 *barometer, uint16_t reg, uint8_t *data)
+{
+	return HAL_I2C_Mem_Read(barometer -> i2cHandle, barometer -> i2c_address, reg, I2C_MEMADD_SIZE_8BIT, data, BMP3_SIZE ,100);
 }
 
-HAL_StatusTypeDef BMP3_read_registers(BMP3 *barometer, uint16_t reg,
-									  uint8_t *data, uint8_t length){
+HAL_StatusTypeDef BMP3_read_registers(BMP3 *barometer, uint16_t reg, uint8_t *data, uint8_t length)
+{
+	return HAL_I2C_Mem_Read(barometer -> i2cHandle, barometer -> i2c_address, reg, I2C_MEMADD_SIZE_8BIT, data, length , 100);
 }
 
-void BMP3_get_temperature(BMP3 *barometer,BMP3_CALIB_DATA *calib_baro){
+
+void BMP3_get_temperature(BMP3 *barometer,BMP3_CALIB_DATA *calib_baro)
+{
+	
 }
 
 void BMP3_get_pressure(BMP3 *barometer,BMP3_CALIB_DATA *calib_baro){
